51f9b27b7e3b58064ff943cb108d4f6f77566305
tenmihi <tenmihi@gmail.com>
2019-01-26 23:21:33 +0900

    master chore(backend, frontend): unito => unicle

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,6 +3,7 @@
     <div class="hero-body">
       <div class="container has-text-centered">
         <img src="../../public/logo.png" width="128" height="80">
+        <p class="description">Untiyに関して話題になってる記事一覧をお届けします</p>
       </div>
     </div>
     <div class="hero-foot">
@@ -22,14 +23,6 @@ import VArticle from '@/components/Article'
 import Pickup from '@/components/Pickup'
 import axios from 'axios'
 
-const dummy = {
-  title: 'TITLE TITLE TITLE',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  url: 'https://jp.vuejs.org/index.html',
-  image: 'https://jp.vuejs.org/images/logo.png',
-  site_name: 'site name dayo'
-}
-
 export default {
   components: {
     VArticle,
@@ -38,7 +31,6 @@ export default {
   data () {
     return {
       items: []
-      //items: [dummy, dummy, dummy, dummy]
     }
   },
   computed: {
@@ -47,8 +39,14 @@ export default {
     }
   },
   async created () {
-    const { data } = await axios.get('https://t8mh9aeqbe.execute-api.ap-northeast-1.amazonaws.com/prod/fetch')
+    const { data } = await axios.get('https://1z0m934jxe.execute-api.ap-northeast-1.amazonaws.com/prod/fetch')
     this.items = data
   }
 }
 </script>
+
+<style scoped>
+.description {
+  color: gray;
+}
+</style>
